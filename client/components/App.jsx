b3bdd057c2cd6e9a17dd17098a6828151f24ac40
Joshua Vial <joshua@enspiral.com>
2017-08-15 09:48:07 +1200

    react lifecycle demo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import Greetings from './Greetings'
 
-const App = () => (
-  <Router>
-    <div className='app-container'>
-      <h1>Hello World</h1>
-      <Route exact path="/" component={Greetings} />
-    </div>
-  </Router>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      person: 'jv'
+    }
+  }
+  randomPerson(e) {
+    const people = ['jv', 'harrison', 'rohan', 'silvia']
+    const index = Math.floor(Math.random() * people.length)
+    this.setState({
+      person: people[index]
+    })
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <Router>
+        <div className='app-container'>
+          <h1>Hello World</h1>
+          <Link to='/somewhere'>Somewhere</Link>
+          <Link to='/'>Home</Link>
+          <Route exact path="/" render={() => <Greetings person={this.state.person}/>} />
+          <button onClick={this.randomPerson.bind(this)}>Change Person</button>
+        </div>
+      </Router>
+    )
+  }
+  componentWillUnmount() {
+      console.log('compoonent did unmount')
+  }
+}
 
 export default App
--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -7,12 +7,45 @@ const renderGreeting = (greeting, key) => (
   <h1 key={key}>{greeting.text}</h1>
 )
 
-const Greetings = ({greetings, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(getGreetings())}>Show Greetings</button>
-    {greetings.map(renderGreeting)}
-  </div>
-)
+class Greetings extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  componentWillReceiveProps(nextProps) {
+      console.log('will receive props', nextProps);
+  }
+  shouldComponentUpdate(nextProps) {
+      console.log('should update', nextProps);
+      return true
+  }
+  componentWillUpdate() {
+      console.log('will update')
+  }
+  componentDidUpdate() {
+      console.log('did update')
+  }
+  render() {
+    console.log('render')
+    const {greetings, dispatch} = this.props
+    return (
+      <div>
+        {this.props.person}
+        <button onClick={() => dispatch(getGreetings())}>Show Greetings</button>
+        {greetings.map(renderGreeting)}
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {greetings: state.greetings}
